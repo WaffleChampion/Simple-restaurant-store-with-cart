fcc13ca2b430900ed1cce2ceba9db1286b0bea75
WaffleChampion <bendikmdelp@gmail.com>
2022-12-07 10:14:08 +0100

    code cleanup and commenting slide-show

--- js/modules/slide-show.js
+++ js/modules/slide-show.js
@@ -14,58 +14,60 @@ export default function slideShow(){
 
 	render();
 	//Eventlisteners
-	if (previousButton && nextButton && slideShowImages){
+	if (previousButton && nextButton && slideShowImages) {
 		previousButton.addEventListener('click', handlePreviousButtonClick);
 		nextButton.addEventListener('click', handleNextButtonClick);
 		setInterval(handleNextButtonClick, 3000);
 	}
 
 	//Handelers
-	function handleNextButtonClick(){
+	function handleNextButtonClick() {
 		increaseIndex();
 		renderImage(slideShowImages, currentIndex);
 	}
 
-	function handlePreviousButtonClick(){
+	function handlePreviousButtonClick() {
 		reduceIndex();
 		renderImage(slideShowImages, currentIndex);
 	}
 
 	//reduces image index
-	function reduceIndex(){
-		if (currentIndex=== 0){
+	function reduceIndex() {
+		if (currentIndex=== 0) {
 			currentIndex = slideShowImages.length-1;
-		}else{
-			currentIndex -=1;
+		} else {
+			currentIndex -= 1;
 		}
 	}
 
 	//Increases image index
-	function increaseIndex(){
-		if (currentIndex === slideShowImages.length-1){
+	function increaseIndex() {
+		if (currentIndex === slideShowImages.length-1) {
 			currentIndex=0; 
-		}else {
-			currentIndex+=1;
+		} else {
+			currentIndex += 1;
 		}
 	}
 
-	function renderImage(slideShowImage, currentIndex){
-		for (let image of slideShowImage){
+	//Display current image
+	function renderImage(slideShowImage, currentIndex) {
+		for (let image of slideShowImage) {
 			image.classList.remove('slide-show__image--active');
 		}
 		slideShowImage[currentIndex].classList.add('slide-show__image--active');
 	}
 
-	function generateSlideShowImages(){
-		if(slideShowImageContainer){
+	//Generates slideshow from menuitems array
+	function generateSlideShowImages() {
+		if (slideShowImageContainer) {
 			menuItems.forEach((item, index)=> {
 				const figureElement = document.createElement('figure');
 				const imageElement = document.createElement('img');
 	
 				figureElement.setAttribute('class', 'slide-show__object');
-				if (index === 0){
+				if (index === 0) {
 					imageElement.setAttribute('class', 'slide-show__image slide-show__image--active');
-				}else {
+				} else {
 					imageElement.setAttribute('class', 'slide-show__image');
 				}
 	
@@ -79,6 +81,7 @@ export default function slideShow(){
 		}
 	}
 
+	//render function
 	function render() {
 		generateSlideShowImages()
 	}
